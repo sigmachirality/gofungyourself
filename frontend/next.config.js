89da91fa1263a82d0a2e62a97f7ea4dfe3f518a3
Owen Simon <osimon922@gmail.com>
2021-12-13 01:46:46 -0500

    public config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,4 +3,8 @@ module.exports = {
   reactStrictMode: true,
   // basePath: process.env.BASE_PATH || "",
   // assetPrefix: process.env.BASE_PATH || ""
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH
+}
 }
