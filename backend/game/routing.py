3621ce8b797068ea46014771e4aa4c5e004fa256
Daniel Tao <danieltaox@gmail.com>
2021-12-12 22:03:29 -0500

    feat: redis layer grouping

--- backend/game/routing.py
+++ backend/game/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/room/<room_name>', consumers.GameConsumer.as_asgi()),
+    path('ws/room/<room_name>/<player>', consumers.GameConsumer.as_asgi()),
 ]
