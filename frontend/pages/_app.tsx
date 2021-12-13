822e3a590a65194bbf857cb3656b09a58d53bb64
Owen Simon <osimon922@gmail.com>
2021-12-13 01:16:00 -0500

    fixed it for real

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log(process.env.BASE_PATH)
   return <Component {...pageProps} />
 }
 
