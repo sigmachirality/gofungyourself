5f3c1139ebb17b22f4023d33a704bb35e5cea3f7
Owen Simon <osimon922@gmail.com>
2021-12-13 05:19:05 -0500

    fixed url

--- frontend/pages/api/[...slug].ts
+++ frontend/pages/api/[...slug].ts
@@ -3,8 +3,8 @@ import httpProxyMiddleware from 'next-http-proxy-middleware'
 
 const middleware = (req: NextApiRequest, res: NextApiResponse) => (
     httpProxyMiddleware(req, res, {
-        target: process.env.BACKEND_URL 
-            ? "https://" + process.env.BACKEND_URL 
+        target: process.env.NEXT_PUBLIC_BACKEND_URL 
+            ? "https://" + process.env.NEXT_PUBLIC_BACKEND_URL 
             : "http://127.0.0.1:8000",
         pathRewrite: [{
           patternStr: '^/api',
