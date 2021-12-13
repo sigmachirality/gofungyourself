df2391af2c831e2a431b5624a4504fed7a48248c
Daniel Tao <danieltaox@gmail.com>
2021-12-12 19:35:49 -0500

    fix: spelling

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -11,12 +11,9 @@ class GameConsumer(WebsocketConsumer):
     def disconnect(self, close_code):
         pass
 
-    def recieve(self, *, text_data):
+    def receive(self, *, text_data):
         # Case on type and handle accordingly
-        print("bruh")
-        print("bruh", text_data)
         text_data_json = json.loads(text_data)
-        print(text_data_json)
         message = text_data_json['message']
 
         self.send(text_data=json.dumps({
